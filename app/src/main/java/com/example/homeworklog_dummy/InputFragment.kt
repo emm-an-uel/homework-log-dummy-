6c32b65df1e6a994cbee0d50b4cded8102976e25
emmanuel <sunartio.emmanuel@gmail.com>
2022-07-15 11:25:48 +0930

    added calendar scroll view but no data stored

--- app/src/main/java/com/example/homeworklog_dummy/InputFragment.kt
+++ app/src/main/java/com/example/homeworklog_dummy/InputFragment.kt
@@ -1,12 +1,18 @@
 package com.example.homeworklog_dummy
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
+import androidx.fragment.app.Fragment
 import com.example.homeworklog_dummy.databinding.FragmentInputBinding
+import android.widget.DatePicker
+import android.app.DatePickerDialog
+import android.widget.EditText
+import android.widget.Toast
+import java.time.format.DateTimeFormatter
+
+import java.util.Calendar
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -32,6 +38,20 @@ class InputFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // declare variables (user input)
+        val subject = binding.subject.text
+        val task = binding.task.text
+        val notes = binding.notes.text
+
+        // datePicker stuff
+        val today = Calendar.getInstance()
+        binding.dueDate.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+        ) { view, year, month, day ->
+                val month = month + 1
+                val msg = "You Selected: $day/$month/$year"
+                Toast.makeText(context, msg, Toast.LENGTH_SHORT).show()
+            }
     }
 
     override fun onDestroyView() {
