ef0f26b6141b702bca60d1a28f9438d5ad2305e6
emmanuel <sunartio.emmanuel@gmail.com>
2022-07-20 16:41:28 +1000

    before trying to store data locally (instead of sending to fragment)

--- app/src/main/java/com/example/homeworklog_dummy/InputFragment.kt
+++ app/src/main/java/com/example/homeworklog_dummy/InputFragment.kt
@@ -1,6 +1,5 @@
 package com.example.homeworklog_dummy
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
--- app/src/main/java/com/example/homeworklog_dummy/LogFragment.kt
+++ app/src/main/java/com/example/homeworklog_dummy/LogFragment.kt
@@ -1,7 +1,6 @@
 package com.example.homeworklog_dummy
 
 import android.app.ActionBar
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -48,13 +47,6 @@ class LogFragment : Fragment() {
             val task = args.task
             val dueDate = args.dueDate
             val notes = args.notes
-
-            // store data locally
-            val filename = "myfile"
-            val fileContents = "subject"
-            context!!.openFileOutput(filename, Context.MODE_PRIVATE).use {
-                it.write(fileContents.toByteArray())
-            }
 
             // create new horizontal linear layout
             val linearLayoutHoriz1 = LinearLayout(context)
