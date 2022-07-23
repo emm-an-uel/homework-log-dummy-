99bfe2e3a2c93ecad9a02f5804b86075d7f2ec4f
emmanuel <sunartio.emmanuel@gmail.com>
2022-07-23 09:50:50 +0930

    clean

---
+++ app/src/main/java/com/example/homeworklog_dummy/InputFragment.kt
@@ -0,0 +1,112 @@
+package com.example.homeworklog_dummy
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.homeworklog_dummy.databinding.FragmentInputBinding
+import java.io.File
+import java.util.*
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class InputFragment : Fragment() {
+
+    private fun sortDueDate(day: Int, month: Int, year: Int): Int {
+        // * dueDateSort will be in format YYYYMMDD for easy sorting of due dates *
+
+        var monthString = month.toString()
+        var dayString = day.toString()
+
+        // ensure proper MM format
+        if (month < 10) {
+            monthString = "0$month" // eg convert "8" to "08"
+        }
+
+        // ensure proper DD format
+        if (day < 10) {
+            dayString = "0$day"
+        }
+
+        // convert to YYYYMMDD format
+        val dateString = "$year$monthString$dayString"
+        val dateInt = dateString.toInt() // return integer so it can be sorted
+
+        return(dateInt)
+    }
+
+    private fun storeLocally(subject : String, task : String, dueDate : String, dateInt: Int) {
+
+        // merge into string
+        val fileContents = "$subject-$task-$dueDate-$dateInt"
+
+        // * store contentToFile into local file *
+
+        // determine number of existing files
+        val files : Array<String> = context!!.fileList()
+        val numFiles = files.size
+
+        // create new file
+        val fileName = "file$numFiles" // eg if there's 1 file, name is "file1"
+        context!!.openFileOutput(fileName, Context.MODE_PRIVATE).use {
+            it.write(fileContents.toByteArray())
+        }
+    }
+
+    private var _binding: FragmentInputBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentInputBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var dueDate = ""
+        var dateInt = 0
+
+        // datePicker stuff
+        val today = Calendar.getInstance()
+        binding.dueDate.init(today.get(Calendar.YEAR), today.get(Calendar.MONTH),
+            today.get(Calendar.DAY_OF_MONTH)
+        ) { view, year, month, day ->
+                val month = month + 1
+                dueDate = "$day $month $year"
+
+                // sort by due date
+                dateInt = sortDueDate(day, month, year)
+            }
+
+        // when button "confirm" is clicked
+        binding.buttonConfirm.setOnClickListener() {
+
+            val subject = binding.subject.text.toString()
+            val task = binding.task.text.toString()
+
+            // stores subject, task, notes in local file
+            storeLocally(subject, task, dueDate, dateInt)
+
+            // navigate to fragment_log
+            findNavController().navigate(InputFragmentDirections.actionInputFragmentToLogFragment())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/example/homeworklog_dummy/LogFragment.kt
@@ -0,0 +1,207 @@
+package com.example.homeworklog_dummy
+
+import android.graphics.Color
+import android.os.Bundle
+import android.view.Gravity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.ViewGroup.LayoutParams.MATCH_PARENT
+import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.homeworklog_dummy.databinding.FragmentLogBinding
+import java.io.File
+import java.util.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class LogFragment : Fragment() {
+
+    private fun markAsDone(subTaskDate: String) {
+        // run through existing files to find the one with content
+        // "subTaskDate" and delete that file
+
+        val files : Array<String> = context!!.fileList()
+        val numFiles = files.size
+        var n = 1
+        while (n < numFiles) {
+            val fileName = "file$n"
+            var fileContents = File(context!!.filesDir, fileName).readText()
+            fileContents = fileContents.dropLast(9)
+
+            if (fileContents == subTaskDate) {
+                context!!.deleteFile(fileName)
+                findNavController().navigate(LogFragmentDirections.actionLogFragmentToStartFragment()) // refresh
+                break
+            }
+
+            n ++
+        }
+    }
+
+    private fun sortAssignments(numFiles: Int): List<String> {
+
+        val assignmentsMap = mutableMapOf<Int, String>() // initialize mutableMap for sorting by due date
+
+        // * access data from local files and put into assignmentsMap *
+        var n = 1 // first file is "rList" so file naming starts with "file1"
+        while (n < numFiles) {
+            val file = File(context!!.filesDir, "file$n").readText() // access each file
+
+            // convert "file" from string to list
+            val list : List<String> = file.split("-").toList()
+
+            // assign variable to each item in list
+            val subject = list[0]
+            val task = list[1]
+            val dueDate = list[2]
+            val dueDateInt = list[3].toInt()
+
+            // put (dueDateInt, subTaskDate) into assignmentsMap
+            val subTaskDate = "$subject-$task-$dueDate"
+            assignmentsMap.put(dueDateInt, subTaskDate)
+
+            n ++
+        }
+
+        // * sort assignmentsMap by due date *
+        val sortedAssignmentsMap: MutableMap<Int, String> = TreeMap(assignmentsMap)
+
+        // * compile sorted values (subTaskDate)'s into a list to be returned *
+        val subTaskDateList: List<String> = sortedAssignmentsMap.values.toList()
+
+        return subTaskDateList // list of "subject-task-dueDate" sorted according to due date
+    }
+
+    private fun displayAssignments(sortedSubTaskDateList: List<String>) {
+        // * access data from local file *
+
+        // display each subTaskDate in a new row
+        var n = 0
+        while (n < sortedSubTaskDateList.size) {
+
+            // convert "sortedSubTaskDateList[n]" from string to list
+            val subTaskDate = sortedSubTaskDateList[n]
+            val list : List<String> = subTaskDate.split("-").toList()
+
+            // assign variable to each item in list
+            val subject = list[0]
+            val task = list[1]
+            val dueDate = list[2]
+
+            // create new horizontal linear layout
+            val linearLayoutHorizontal = LinearLayout(context)
+            linearLayoutHorizontal.layoutParams = LinearLayout.LayoutParams(
+                MATCH_PARENT,
+                0,
+                4F // weight 4 to fit 3 text views + 1 button
+            )
+            linearLayoutHorizontal.orientation = LinearLayout.HORIZONTAL // horizontal layout
+            binding.linearLayout.addView(linearLayoutHorizontal) // add horizontal layout to parent vertical layout
+
+            // create new text views
+            val textViewSubject = TextView(context)
+            val textViewTask = TextView(context)
+            val textViewDueDate = TextView(context)
+
+            // * populate text views with local data retrieved above *
+            textViewSubject.text = subject
+            textViewSubject.textSize = 18F
+            textViewSubject.gravity = Gravity.CENTER
+            textViewSubject.layoutParams = LinearLayout.LayoutParams(
+                MATCH_PARENT,
+                MATCH_PARENT,
+                1F // weight is set to 1F so all 3 text views are evenly distributed to take up the whole row
+            )
+
+            textViewTask.text = task
+            textViewTask.textSize = 18F
+            textViewTask.gravity = Gravity.CENTER
+            textViewTask.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            textViewTask.layoutParams = LinearLayout.LayoutParams(
+                MATCH_PARENT,
+                MATCH_PARENT,
+                1F
+            )
+
+            textViewDueDate.text = dueDate
+            textViewDueDate.textSize = 18F
+            textViewDueDate.gravity = Gravity.CENTER
+            textViewDueDate.textAlignment = View.TEXT_ALIGNMENT_CENTER
+            textViewDueDate.layoutParams = LinearLayout.LayoutParams(
+                MATCH_PARENT,
+                MATCH_PARENT,
+                1F
+            )
+
+            // * create button to mark as done *
+            val buttonDone = Button(context)
+            buttonDone.setOnClickListener() {
+                markAsDone(subTaskDate)
+            }
+            buttonDone.layoutParams = LinearLayout.LayoutParams(
+                MATCH_PARENT,
+                MATCH_PARENT,
+                1F
+            )
+            buttonDone.text = "done"
+            buttonDone.textSize = 13F
+            buttonDone.setTextColor(Color.WHITE)
+
+            // display 3 columns of info + 1 button
+            linearLayoutHorizontal.addView(textViewSubject)
+            linearLayoutHorizontal.addView(textViewTask)
+            linearLayoutHorizontal.addView(textViewDueDate)
+            linearLayoutHorizontal.addView(buttonDone)
+
+            n++
+        }
+    }
+
+    private var _binding: FragmentLogBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentLogBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // * display assignments *
+        // determine number of existing files
+        val files : Array<String> = context!!.fileList()
+        val numFiles = files.size
+
+        if (numFiles > 1) { // first file is rList which does not have items as required by displayAssignments
+            
+            val sortedSubTaskDateList = sortAssignments(numFiles) // this should return a sorted list
+            displayAssignments(sortedSubTaskDateList) // pass a sorted list to the function, function displays each item in fragment_log
+        }
+
+        // button to create new assignment
+        binding.newAssignment.setOnClickListener {
+            findNavController().navigate(LogFragmentDirections.actionLogFragmentToInputFragment())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
